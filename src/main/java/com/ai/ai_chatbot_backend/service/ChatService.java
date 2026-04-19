26a35882dbbd39f1d5f19ecb19d01c5e38885998
Srinvas-dappu <srinivas64400@gmail.com>
2026-04-19 12:42:48 +0530

    added sdk

--- src/main/java/com/ai/ai_chatbot_backend/service/ChatService.java
+++ src/main/java/com/ai/ai_chatbot_backend/service/ChatService.java
@@ -1,13 +1,28 @@
 package com.ai.ai_chatbot_backend.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentConfig;
+import com.google.genai.types.GenerateContentResponse;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.stereotype.Service;;
 
 @Service
 public class ChatService {
 
+
+    public String getResponse(String userMessage){
+       Client client=new Client();
+
+       GenerateContentConfig config=GenerateContentConfig
+               .builder()
+               .temperature(0.9F)
+               .maxOutputTokens(1000)
+               .build();
+
+       GenerateContentResponse response=client
+               .models.generateContent("gemini-3-flash-preview",userMessage,config);
+
+       return  response.text();
+    }
 }
